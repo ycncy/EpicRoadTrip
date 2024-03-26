d8f4ba9f56fac929331b48ecdeecdf4ce3d0dd17
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2024-03-26 15:23:42 +0100

    [fix] css

--- front/app/components/Map.tsx
+++ front/app/components/Map.tsx
@@ -5,10 +5,7 @@ import 'leaflet/dist/leaflet.css';
 import {Trip} from "@/app/lib/model/Trip";
 import {TripStop} from "@/app/lib/model/TripStop";
 import {tripService} from "@/app/lib/service/trip.service";
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import * as L from 'leaflet';
-import 'leaflet-defaulticon-compatibility';
 import Icon from "../public/images/marker.png";
 
 
@@ -34,28 +31,37 @@ const Map = (props: {tripId: string}) => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker
-                    position={[trip.startPosition.latitude, trip?.startPosition.longitude]}
-                    icon={L.icon({
-                        iconSize: [36, 36],
-                        iconUrl: Icon.src
-                    })}
-                >
-                    <Popup>
-                        Point de départ de votre Road Trip
-                    </Popup>
-                </Marker>
-                <Marker
-                    position={[trip.endPosition.latitude, trip.endPosition.longitude]}
-                    icon={L.icon({
-                        iconSize: [36, 36],
-                        iconUrl: Icon.src
-                    })}
-                >
-                    <Popup>
-                        Point de départ de votre Road Trip
-                    </Popup>
-                </Marker>
+                {
+                    trip.startPosition.latitude && trip?.startPosition.longitude && (
+                        <Marker
+                            position={[trip.startPosition.latitude, trip?.startPosition.longitude]}
+                            icon={L.icon({
+                                iconSize: [36, 36],
+                                iconUrl: Icon.src
+                            })}
+                        >
+                            <Popup>
+                                Point de départ de votre Road Trip
+                            </Popup>
+                        </Marker>
+                    )
+                }
+                {
+                    trip.endPosition.latitude && trip.endPosition.longitude && (
+                        <Marker
+                            position={[trip.endPosition.latitude, trip.endPosition.longitude]}
+                            icon={L.icon({
+                                iconSize: [36, 36],
+                                iconUrl: Icon.src
+                            })}
+                        >
+                            <Popup>
+                                Point de départ de votre Road Trip
+                            </Popup>
+                        </Marker>
+                    )
+                }
+
             </MapContainer> : <></>
     );
 };
