26a57893f07dd5e0387ae873d7cdd22e64987b09
Meriem <ramdani.meriem2001@gmail.com>
2024-04-08 11:21:13 +0200

    add login form

---
+++ front/app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "../components/LoginForm/LoginForm";
+
+export default function LoginPage() {
+    return (
+        <main>
+            <h1>Login</h1>
+            <LoginForm />
+        </main>
+    )
+}
