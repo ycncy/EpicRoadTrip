7cad9774a9601467e29eccb15c0a848f116cf360
Meriem <ramdani.meriem2001@gmail.com>
2024-05-07 10:55:41 +0200

    delete pycach

--- front/app/components/SavedTrips/SavedTrips.tsx
+++ front/app/components/SavedTrips/SavedTrips.tsx
@@ -27,7 +27,9 @@ const SavedTrips = () => {
                 {savedTrips.map((trip, index) => (
                     <div key={index} className="max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{trip.title}</h5>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{trip.description}</p>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{trip.start_position} - {trip.end_position} </p>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{trip.start_datetime} - {trip.end_datetime} </p>
+
                         <a href="#" className="inline-flex font-medium items-center text-blue-600 hover:underline">
                             Voir sur la carte
                             <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
@@ -48,7 +50,7 @@ const SavedTrips = () => {
                     </div>
                     <div  className="max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">exemple</h5>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">aa</p>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lieu de depart- lieu d'arrivé</p>
                         <a href="#" className="inline-flex font-medium items-center text-blue-600 hover:underline">
                             Voir sur la carte
                             <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
@@ -69,7 +71,7 @@ const SavedTrips = () => {
                     <div  className="max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">exemple</h5>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">aa</p>
-                        <a href="#" className="inline-flex font-medium items-center text-blue-600 hover:underline">
+                        <a href="/"  className="inline-flex font-medium items-center text-blue-600 hover:underline">
                             Voir sur la carte
                             <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
